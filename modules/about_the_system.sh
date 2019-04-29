b117bc99f27417aaedd28863bf528b309e6373c7
450RemoteDeployment <remotedeploys@securenet.systems>
2019-04-29 22:36:58 +0100

    Added the nordvpn status checker and formatted the code

--- modules/about_the_system.sh
+++ modules/about_the_system.sh
@@ -8,3 +8,12 @@ function find_system_installtion_time(){
 sudo ls -alct /|tail -1|awk '{print $6, $7, $8}'
 }
 
+#Other information about the system
+function call_system_report(){
+	lscpu
+}
+
+function system_report(){
+	find_system_installtion_time
+	call_system_report
+}
--- ubuntnoob.sh
+++ ubuntnoob.sh
@@ -28,23 +28,6 @@ function display_output(){
 	dialog --backtitle "UbuntuNoob" --title "${t}" --clear --msgbox "$(<$OUTPUT)" ${h} ${w}
 }
 
-
-#
-# Purpose - display current system date & time
-#
-function show_date(){
-	echo "Today is $(date) @ $(hostname -f)." >$OUTPUT
-    display_output 6 60 "Date and Time"
-}
-#
-# Purpose - display a calendar
-#
-function show_calendar(){
-	cal >$OUTPUT
-	display_output 13 25 "Calendar"
-}
-
-
 #
 # set infinite loop
 #
@@ -73,11 +56,11 @@ function end_applcaiton_installation(){
 
 ##BEGIN SUDO CHECK FUNCTION##
 function check_for_root_privilages(){
-if [[ $EUID -ne 0 ]]; then
-   sudo -k
-    pass=$(whiptail --backtitle "$app_name" --title "Authentication required" --passwordbox "$app_name requires sudo.\n To continue please authenticate for user $USER:" 12 50 3>&2 2>&1 1>&3-)
-    exec sudo -S -p '' "$0" "$@" <<< "$pass"
-fi
+	if [[ $EUID -ne 0 ]]; then
+	   sudo -k
+	    pass=$(whiptail --backtitle "$app_name" --title "Authentication required" --passwordbox "$app_name requires sudo.\n To continue please authenticate for user $USER:" 12 50 3>&2 2>&1 1>&3-)
+	    exec sudo -S -p '' "$0" "$@" <<< "$pass"
+	fi
 }
 ##BEGIN SUDO CHECK FUNCTION##
 
@@ -94,27 +77,27 @@ function run_rkhunter(){
 }
 
 function check_rkhunter_installed(){
-application_to_install="rkhunter"
-if ! [ -x "$(command -v " $application_to_install")"]; then
-		 echo 'Error: '$application_to_install 'is not installed.' >&2
-		 echo 'Now installing '$application_to_install
-		 sudo apt-get install -y $application_to_install
-	 		else
-	 			run_rkhunter
-fi 
+	application_to_install="rkhunter"
+		if ! [ -x "$(command -v " $application_to_install")"]; then
+				 echo 'Error: '$application_to_install 'is not installed.' >&2
+				 echo 'Now installing '$application_to_install
+				 sudo apt-get install -y $application_to_install
+			 		else
+			 			run_rkhunter
+		fi 
 }
 
 ##END RKHUNTER##
 
 ##BEGIN FORCE END APPLICATION##
 function end_application(){
-echo "Kill Process (PID): "
-read -r prkill
-scriptname=`basename "$prkill"`
-pgrep $scriptname
-pkill -9 $scriptname
-echo $scriptname" Has been terminated"
-exit 1
+	echo "Kill Process (PID): "
+	read -r prkill
+	scriptname=`basename "$prkill"`
+	pgrep $scriptname
+	pkill -9 $scriptname
+	echo $scriptname" Has been terminated"
+	exit 1
 }
 ##END FORCE END APPLICATION##
 
@@ -136,29 +119,43 @@ function system_report(){
 
 ##END ABOUT THE SYSTEM##
 
+#BEGIN CURRENT DATE##
+function show_date(){
+	echo "Today is $(date) @ $(hostname -f)." >$OUTPUT
+    display_output 6 60 "Date and Time"
+}
+##END CURRENT DATE##
+
+#BEGIN SHOW CALENDAR##
+function show_calendar(){
+	cal >$OUTPUT
+	display_output 13 25 "Calendar"
+}
+##END SHOW CALENADR##
+
 ##BEGIN INSTALL TEAMVIEWER##
 
 #Create teamviewer temp install file
 function teamv_create_temp_folder(){
-cd $HOME
-dir=.$RANDOM
-mkdir $dir
-cd $dir
+	cd $HOME
+	dir=.$RANDOM
+	mkdir $dir
+	cd $dir
 }
 
 #Check the debian version
 function teamv_download_installer(){
-arch=`uname -m`
-if [ "$arch" == "x86_64" ]
-then
-    echo "Downlaoding the 64 Bit Installer"
-    version="teamviewer_amd64.deb"
-    wget https://download.teamviewer.com/download/linux/$version
-else
-    echo "Downlaoding the 32 Bit Installer"
-    version="teamviewer_i386.deb"
-    wget https://download.teamviewer.com/download/linux/$version
-fi
+	arch=`uname -m`
+		if [ "$arch" == "x86_64" ]
+		then
+		    echo "Downlaoding the 64 Bit Installer"
+		    version="teamviewer_amd64.deb"
+		    wget https://download.teamviewer.com/download/linux/$version
+		else
+		    echo "Downlaoding the 32 Bit Installer"
+		    version="teamviewer_i386.deb"
+		    wget https://download.teamviewer.com/download/linux/$version
+		fi
 }
 
 function make_executable(){
@@ -194,18 +191,18 @@ function local_user_accounts(){
 
 #List all current sessions
 function logged_in_users(){
-echo
-echo "All Connected Clients"
-echo
-who -a
+	echo
+	echo "All Connected Clients"
+	echo
+	who -a
 
 }
 
 function current_ssh_connections(){
-echo
-echo "SSH Clients"
-echo
-sudo netstat -tnpa | grep 'ESTABLISHED.*sshd'
+	echo
+	echo "SSH Clients"
+	echo
+	sudo netstat -tnpa | grep 'ESTABLISHED.*sshd'
 }
 
 function all_users(){
@@ -256,28 +253,28 @@ function core_app_env_check(){
 }
 
 function easy_apt(){
-apt="sudo apt-get -qq "
-install="install -y"
-remove="remove -y"	
+	apt="sudo apt-get -qq "
+	install="install -y"
+	remove="remove -y"	
 }
 
 function update_repos(){
-$apt update
-clear
+	$apt update
+	clear
 }
 
 function core_app_array_declare(){
-# declare names as an indexed array
-declare -a install_core_system_applications
-declare -a applcations_installed
+	# declare names as an indexed array
+	declare -a install_core_system_applications
+	declare -a applcations_installed
 }
 
 
 # initialize the array
 function core_app_fill_array(){
-install_core_system_applications=("htop" "secure-delete" "wget" "pgpgpg")
-core_applications_explained=("A great Task Manager" "A very secure file remover" "Allows for the download of files from the internet" "Used to create and manage encrytion keys")
-applcations_installed=()
+	install_core_system_applications=("htop" "secure-delete" "wget" "curl" "pgpgpg")
+	core_applications_explained=("A great Task Manager" "A very secure file remover" "Allows for the download of files from the internet" "Another application for getting things from the internet" "Used to create and manage encrytion keys")
+	applcations_installed=()
 }
 
 function install_core_system_apps(){ 
@@ -293,23 +290,23 @@ for i in "${install_core_system_applications[@]}"
  }
 
 function core_install(){
-core_app_env_check
-easy_apt
-update_repos
-core_app_array_declare
-core_app_fill_array
-install_core_system_apps
+	core_app_env_check
+	easy_apt
+	update_repos
+	core_app_array_declare
+	core_app_fill_array
+	install_core_system_apps
 }
 ##END CORE APP INSTALL FUNCTION##
 
 ##BEGIN CHECK FOR SECURE-DELETE FUNCTION##
 function check_for_secure_delete(){
- if ! [ -x "$(command -v srm)" ]; then
- echo 'Error: Secure Delete is not installed.' >&2
- echo 'Secure Delete is an essential part of the $programname'
- echo 'Now installing Secure Delete'
- sudo apt-get install secure-delete -y
-fi 
+	 if ! [ -x "$(command -v srm)" ]; then
+		 echo 'Error: Secure Delete is not installed.' >&2
+		 echo 'Secure Delete is an essential part of the $programname'
+		 echo 'Now installing Secure Delete'
+		 sudo apt-get install secure-delete -y
+	fi 
  }
  ##END CHECK FOR SECURE-DELETE FUNCTION##
 
@@ -336,10 +333,10 @@ function update(){
  }
  
 function system_update(){
-system_update_check_env
-update
-upgrade
-update_cleanup
+	system_update_check_env
+	update
+	upgrade
+	update_cleanup
 }
 #END SYSTEM UPDATE FUNCTION#
 
@@ -363,9 +360,9 @@ function bash_sanatise(){
 }
 
 function prevent_bash_history(){
-echo '$app_name is currently preventing any further recording of your Bash history '
-echo 'unset HISTFILE' >> ~/.bashrc
-echo 'export LESSHISTFILE="-"' >> ~/.bashrc
+	echo '$app_name is currently preventing any further recording of your Bash history '
+	echo 'unset HISTFILE' >> ~/.bashrc
+	echo 'export LESSHISTFILE="-"' >> ~/.bashrc
 }
 
 #Clear the current content
@@ -479,13 +476,13 @@ function about_nord(){
 
 ##BEGIN INSTALL SUBLIME TEXT EDITOR FUNCTION##
 #Installation variables
-	function sublime_env_check(){
-	check_for_root_privilages
+function sublime_env_check(){
 	application_ident="Sublime Text Editor"
 	app_name=$application_ident
 	temp_dir="$application_ident_config_dir"
 	installation_attempted=0
 	installation_completed=0
+	check_for_root_privilages
 }
 
 function check_for_sublime(){
@@ -503,11 +500,11 @@ function install_app(){
 	echo "Would you like to install" $application_to_install "? [Y/N]"
 	read make_install
 	echo ""
-if [[ "$make_install" =~ (yes|y|Y)$ ]]; then
-			sublime_setup
-	else
-		echo "The installation has been cancelled"
-fi
+		if [[ "$make_install" =~ (yes|y|Y)$ ]]; then
+					sublime_setup
+			else
+				echo "The installation has been cancelled"
+		fi
 }
 
 
@@ -527,55 +524,55 @@ function choose_release(){
 	echo "#####################################################"
 	read stable_relase
 	echo ""
-	if [[ "$stable_relase" =~ (yes|y|Y)$ ]]; then
-		echo "Installing the stable release for "$application_ident
-		echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-	else
-		echo "Install the development release for "$application_ident
-		echo "deb https://download.sublimetext.com/ apt/dev/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+		if [[ "$stable_relase" =~ (yes|y|Y)$ ]]; then
+			echo "Installing the stable release for "$application_ident
+			echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+		else
+			echo "Install the development release for "$application_ident
+			echo "deb https://download.sublimetext.com/ apt/dev/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 
-	fi
+		fi
 }
 
 function install_sublime(){
-sudo apt-get install -y sublime-text 
+	sudo apt-get install -y sublime-text 
 }
 
 function add_sublime_as_executable(){
-if [ ! -f /usr/local/bin/sublime_text ]; then
-			echo ""
-	    	echo "Sublime has not been added as a BASH executable file"
-	    	echo "Now adding Sublime - You can execute it using the command 'subl'"
-	    	sudo ln -s /opt/sublime_text/sublime_text /usr/local/bin/sublime_text
-	    	echo ""
-	    	echo "Sublime is now a bash executable application"
-		else
-			echo ""
-			echo "The sublime executable has been found and no further action is required"
-			echo "You can execute it using the command 'subl'"
-	fi
+	if [ ! -f /usr/local/bin/sublime_text ]; then
+				echo ""
+		    	echo "Sublime has not been added as a BASH executable file"
+		    	echo "Now adding Sublime - You can execute it using the command 'subl'"
+		    	sudo ln -s /opt/sublime_text/sublime_text /usr/local/bin/sublime_text
+		    	echo ""
+		    	echo "Sublime is now a bash executable application"
+			else
+				echo ""
+				echo "The sublime executable has been found and no further action is required"
+				echo "You can execute it using the command 'subl'"
+		fi
 }
 
 function use_sublime_as_default(){
 	echo -n "Would you like to use Sublime as your Default text editor (Y/N)?"
 	read sublime_default
 	echo
-	if [[ "$sublime_default" =~ (yes|y|Y)$ ]]; then
-		echo $application_ident " has been configured as the deafult text editor"
-		sudo sed -i 's/gedit/sublime_text/g' /usr/share/applications/defaults.list
-	else
-		echo "The installation has not made "$application_ident " your default text editor"
-	fi
+		if [[ "$sublime_default" =~ (yes|y|Y)$ ]]; then
+			echo $application_ident " has been configured as the deafult text editor"
+			sudo sed -i 's/gedit/sublime_text/g' /usr/share/applications/defaults.list
+		else
+			echo "The installation has not made "$application_ident " your default text editor"
+		fi
 }
 
 function sublime_setup(){
 	sublime_env_check
-	check_for_root_privilages #Called from functions
+	check_for_root_privilages
 	check_for_sublime
 	install_GPG_key
 	allow_https_sources
 	choose_release
-	update #Called from functions
+	update
 	install_sublime
 	echo ""
 	echo $application_ident " has now been installed."
@@ -588,33 +585,87 @@ function sublime_setup(){
 
 ##BEGIN CHECK SITE STATUS FUNCTION##
 function get_address_to_check(){
-#Get the URL from the user
-echo "Enter URL:" 
-read -r url
+	#Get the URL from the user
+	echo "Enter URL:" 
+	read -r url
 }
 
 
 function check_connection(){
-
-ping -q -c5 -w 1 $url  #Ping the site
-echo "Now checking the connection to $url"
-if [ $? -eq 0 ] #If the conn is successful
-then
-	echo
-	echo
-    echo "Connected to "$url
-else
-	echo #The ping request will leave an error line - This is simply to make it prett
-    echo "Error: no ping returned from "$url
-    echo 
-    echo "I was unable to connect to "$url
-    echo "Please check your connection to the internet."
-    #add link to ipv4 checker
-fi
+	ping -q -c5 -w 1 $url  #Ping the site
+	echo "Now checking the connection to $url"
+		if [ $? -eq 0 ] #If the conn is successful
+		then
+			echo
+			echo
+		    echo "Connected to "$url
+		else
+			echo #The ping request will leave an error line - This is simply to make it prett
+		    echo "Error: no ping returned from "$url
+		    echo 
+		    echo "I was unable to connect to "$url
+		    echo "Please check your connection to the internet."
+		    #add link to ipv4 checker
+		fi
 }
 ##END CHECK SITE STATUS FUNCTION##
 
+##BEGIN NORDVPN CONNECTION CHECK##
+function get_nord_status(){
+	#NEED TO ADD A CHECK THAT NORD IS ACTUALLY INSTALLED
+nordvpn status
+}
 
+function list_nord_options(){
+nordvpn settings list
+}
+
+function wan_ip(){
+	wan_file_name="WanIPPInaW.txt"
+	curl -s ifconfig.me/ip > $wan_file_name
+	wan_ip=$(<$wan_file_name)
+	echo $wan_ip
+	srm $wan_file_name
+}
+
+
+function check_IPV4(){
+	ip_add="1.1.1.1"
+	ip_sp="Cloudflare"
+		if ping -q -c 1 -W 1 $ip_add >/dev/null; then
+		  echo "$ip_sp IPv4 DNS Server is up"
+		else
+		  echo "$ip_sp IPv4 DNS Server is down"
+		fi
+}
+
+function check_IPV4_dns(){
+	url="a4b.us-east-1.amazonaws.com"
+	urlservername="AmazonAWS"
+		if ping -q -c 1 -W 1 a4b.us-east-1.amazonaws.com >/dev/null; then
+		  echo "$urlservername network is up"
+		else
+		  echo "$urlservername network is down"
+		fi
+}
+
+function about_nord(){
+	echo "Current NordVPN status"
+	get_nord_status
+	echo ""
+	echo "NordVPN Settings"
+	list_nord_options
+	echo ""
+	echo "Your WAN IP"
+	wan_ip
+	echo ""
+	echo "Checking your connection to the IPV4 network"
+	check_IPV4
+	echo ""
+	echo "Checking your connection to DNS Server"
+	check_IPV4_dns
+}
+##END NORDVPN CONNECTION CHECK##
 
 
 
